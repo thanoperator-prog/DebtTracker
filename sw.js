356b278eaf465b1935106676f56657654c2959cd
thanoperator-prog <thanoperator@gmail.com>
2025-12-26 17:59:20 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'debt-tracker-v1';
+const CACHE_NAME = 'debt-tracker-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
-  // External Libraries (CDNs) used in your app
+  // External Libraries (CDNs)
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/@babel/standalone/babel.min.js',
   'https://esm.sh/react@18.2.0',
@@ -14,32 +14,29 @@ const ASSETS_TO_CACHE = [
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap'
 ];
 
-// Install Event: Cache files
+// Install Event
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching all assets');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// Activate Event: Cleanup old caches
+// Activate Event: Clean up old caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
 
-// Fetch Event: Serve from Cache, fall back to Network
+// Fetch Event: Serve from cache, fall back to network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
